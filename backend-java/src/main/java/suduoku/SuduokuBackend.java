cc1d7b99be473664a122dd591512b0639351a63a
Christopher Andrade <andralug01@gmail.com>
2025-04-19 06:39:00 -0700

    server print

--- backend-java/src/main/java/suduoku/SuduokuBackend.java
+++ backend-java/src/main/java/suduoku/SuduokuBackend.java
@@ -10,7 +10,7 @@ public class SuduokuBackend {
         String port = System.getenv("PORT");
         int serverPort = (port != null) ? Integer.parseInt(port) : 8080; // Default to 8080 if PORT is not set
         Server server = new Server("0.0.0.0", serverPort, "/", null, WebSocketServer.class);
- 
+        System.out.println("Starting server on port " + serverPort);
         try {
             server.start();          
 
